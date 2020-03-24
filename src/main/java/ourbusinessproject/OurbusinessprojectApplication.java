dc0bb0543cd7480f274cfd4d704e4fff147bdc59
Matthiou <matthieu.batifol@gmail.com>
2020-03-24 16:11:41 +0100

    fix #2.1.10 non regression OK

--- src/main/java/ourbusinessproject/OurbusinessprojectApplication.java
+++ src/main/java/ourbusinessproject/OurbusinessprojectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OurbusinessprojectApplication {
-
+    // fix #2.1.10 non regression OK
     public static void main(String[] args) {
         SpringApplication.run(OurbusinessprojectApplication.class, args);
     }
